26c4651cf125149dedcf0c466a5b62f297f288ff
aimenSaf <i210588@nu.edu.pk>
2025-05-12 01:38:43 +0500

    added updated index.js

--- task-manager/backend/index.js
+++ task-manager/backend/index.js
@@ -17,6 +17,11 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use('/api/tasks', taskRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Backend is live and running on EC2!");
+});
+
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
